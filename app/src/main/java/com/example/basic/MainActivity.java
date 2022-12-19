2950d748c6bafdacedc599dbe150916fe3c482f0
IronoaNik <48927060+IronoaNik@users.noreply.github.com>
2022-12-19 18:29:49 +0000

    Logcat

--- app/src/main/java/com/example/basic/MainActivity.java
+++ app/src/main/java/com/example/basic/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.basic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("success", "Button disabled");
     }
 
-    //We can work with the button by using the variable v.
-    public void disable(View v){
-        Button button = findViewById(R.id.toDoListButton);
-        button.setEnabled(false);
-        button.setText("Disabled");
+    public void handleText(View v){
+        EditText editText = findViewById(R.id.source);
+        String input = editText.getText().toString();
+
 
     }
-
-
 }
