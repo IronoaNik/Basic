6822f476c180f368e423637136acfd7b3dc558e0
IronoaNik <48927060+IronoaNik@users.noreply.github.com>
2023-01-06 14:48:42 +0000

    New Page Added

--- app/src/main/java/com/example/basic/MainActivity.java
+++ app/src/main/java/com/example/basic/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
-import org.w3c.dom.Text;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("success", "Button disabled");
     }
 
     public void handleText(View v){
         EditText editText = findViewById(R.id.source);
         String input = editText.getText().toString();
 
+        TextView textView = findViewById(R.id.output);
+        textView.setText(input);
+        Toast.makeText(this, "Alert", Toast.LENGTH_LONG).show();
+    }
+
+    public void disable(View v){
+        findViewById(R.id.button).setEnabled(false);
+        ((Button)findViewById(R.id.button)).setText("Disabled");
+    }
+
+    //launch a new activity
+    public void launchPage(View v){
+        Intent i = new Intent(this, NewPageActivity.class);
+        startActivity(i);
 
     }
+
 }
